2ffcfd4c1b6a0e0e1fae1e0d414110c46f1aba82
hemantsingh5151 <hemantsingh5151@gmail.com>
2020-01-20 11:16:31 +0530

    Updates

---
+++ precache-manifest.74e01552f96f16ef33ed4ac2c408a35b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e64328c1959801f886a12081dd8088",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "1cd03332c68c20822268",
+    "url": "/monsters/static/css/main.308c3000.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cd03332c68c20822268",
+    "url": "/monsters/static/js/main.6a082b69.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
